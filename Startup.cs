eaaa45f74727c7ab7b09ae154b26376174d4e1ef
Garviell <faustusxi@gmail.com>
2014-04-30 14:41:52 +0000

    fgffg

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testble.Startup))]
+namespace Testble
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
